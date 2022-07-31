7163187eeb2d909e9afa0974e7958ee12e856aa0
esaracho <sarachoesteban@gmail.com>
2022-07-31 01:14:24 -0300

    Se agregan traducciones de algunos productos

--- src/productos-bd.ts
+++ src/productos-bd.ts
@@ -13,19 +13,17 @@ export interface Productos {
       nombre: `Fernet Stock`,
       img:`./assets/img/fernet-stock.jpg`,
       precio: 799,
-      descripcion: `Un fernet horrible, destapa caños`,
-      detalle: `Fernet Stock es un licor o aguardiente aromatizado, color caramelo y gusto amargo elaborado en Italia, Argentina y República Checa, 
-      con un contenido en alcohol de 40 a 41 %, elaborado a partir de una mezcla de 14 hierbas diferentes originarias de los Alpes y 
-      del entorno mediterráneo.`
+      descripcion: `Bad but cheap Fernet`,
+      detalle: `Fernet Stock is a flavored liquor, caramel-colored and bitter taste made in Italy, Argentina and Czech Republic, 
+      with an alcohol content of 40% to 41%, made from a blend of 14 different herbs from the Alps and the Mediterranean Sea area.`
     },
     {
       id: 2,
-      nombre: `Cerveza Diosa Tropical`,
+      nombre: `Diosa Tropical Beer`,
       img:`./assets/img/cerveza-diosa.jpg`,
       precio: 250,
-      descripcion: `¡Cumbia nena!`,
-      detalle: `La Diosa Tropical fue una marca de cerveza elaborada por Isenbeck destinado para el consumo del público aficionado
-      a la bailanta y la cumbia. `
+      descripcion: `Cumbia babe!`,
+      detalle: `Diosa Tropical was a brand of beer brewed by Isenbeck for the consumption of the public fond of tropical dancing and cumbia. `
     },
     {
       id: 3,
@@ -33,9 +31,8 @@ export interface Productos {
       img:`./assets/img/hesperidina.jpg`,
       precio: 450,
       descripcion: `El trago de los viejos`,
-      detalle: `La Hesperidina es una bebida argentina a base de corteza de naranjas amargas o agrias y dulces de frutos inmaduros los cuales tienen
-       gran contenido de flavonoides (hesperidina, neohesperidina y narangina). Su sabor es suave, dulce, y se suele mezclar con agua tónica,
-        agua gaseosa, gaseosa de lima y limón, o como mezcla en diferentes tragos o cócteles.`
+      detalle: `La Hesperidina es una bebida argentina a base de corteza de naranjas amargas o agrias y dulces de frutos inmaduros. Su sabor es suave,
+       dulce, y se suele mezclar con agua tónica, agua gaseosa, gaseosa de lima y limón, o como mezcla en diferentes tragos o cócteles.`
     },
     {
       id: 4,
@@ -52,7 +49,7 @@ export interface Productos {
       img:`./assets/img/amargo-obrero.webp`,
       precio: 450,
       descripcion: `El trago del proletariado`,
-      detalle: `El Amargo Obrero es una bebida inventada en la ciudad de Rosario, Santa Fe, en el año 1888, como una mezcla de hierbas aromáticas 
+      detalle: `Amargo Obrero es una bebida inventada en la ciudad de Rosario, Santa Fe, en el año 1888, como una mezcla de hierbas aromáticas 
       serranas. Lo que lo postula como una “bebida del pueblo” es su finalidad original: erigirse en reacción amarga ante las bebidas dulces que 
       tomaban las clases burguesas. `
     },
