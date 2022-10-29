ee63cf57ea8f82c5f935eb544a72b0dcb67207e0
flavioucp <flavioucp@hotmail.com>
2022-10-29 20:15:15 -0300

    admin dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetalleProductoComponent } from './pages/detalle-producto/detalle-producto.component';
 import { ListaProductosComponent } from './pages/lista-productos/lista-productos.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
 import { InicioSesionComponent } from './pages/inicio-sesion/inicio-sesion.component';
+import { AdminProductoComponent } from './pages/admin-producto/admin-producto.component';
+
 
 const   ROUTES: Routes = [
   {path: 'inicio', component: ListaProductosComponent},
@@ -12,7 +14,10 @@ const   ROUTES: Routes = [
   {path: 'registro', component: RegistroComponent},
   {path: 'quienes-somos', component: QuienesSomosComponent},
   {path: 'inicio-sesion', component: InicioSesionComponent},
+  {path: 'admin-producto',component:AdminProductoComponent},
+ 
   {path:'**', pathMatch: 'full', redirectTo: 'inicio'}
+ 
 
 ];
 
