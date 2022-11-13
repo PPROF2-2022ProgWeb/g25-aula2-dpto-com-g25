af020f2d89529a871cd4d38cf5dd22b9cc0a2c8c
esaracho <sarachoesteban@gmail.com>
2022-11-13 17:33:41 -0300

    Se implementa alta producto, correcciones menores en backend

--- api-spring/escabio/src/main/java/g25/escabio/security/WebSecurityConfig.java
+++ api-spring/escabio/src/main/java/g25/escabio/security/WebSecurityConfig.java
@@ -86,7 +86,8 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
     http.cors().and().csrf().disable()
         .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-        .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+        .authorizeRequests().antMatchers("/api/auth/**", "/api/productos/**").permitAll()
+        //.authorizeRequests().antMatchers("/api/productos/**").permitAll()
         .antMatchers("/api/**").permitAll()
         .anyRequest().authenticated();
     
