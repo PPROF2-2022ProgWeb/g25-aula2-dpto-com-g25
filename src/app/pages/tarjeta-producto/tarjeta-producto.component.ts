aed0201acdd748adcecc595cc2c730175c5c3780
esaracho <sarachoesteban@gmail.com>
2022-11-15 22:05:38 -0300

    Se agrega ngIf para que solo muestre el botón comprar a los usuarios logueados

--- src/app/pages/tarjeta-producto/tarjeta-producto.component.ts
+++ src/app/pages/tarjeta-producto/tarjeta-producto.component.ts
@@ -1,6 +1,12 @@
-import {Component, Input } from '@angular/core';
+import {Component, Input, OnInit } from '@angular/core';
 import { Producto } from 'src/app/models/producto.model';
 import { CarritoService } from 'src/app/services/carrito.service';
+import { LoginDataService } from 'src/app/services/login-data.service';
+import { TokenStoreService } from 'src/app/services/token-store.service';
+
+import { Subscription } from 'rxjs';
+
+
 
 
 
@@ -11,9 +17,21 @@ import { CarritoService } from 'src/app/services/carrito.service';
    })
 
 
-   export class TarjetaProductoComponent{
+   export class TarjetaProductoComponent implements OnInit{
       
-    constructor(private carritoService: CarritoService) {
+    isLoggedIn!: boolean;
+    subscription!: Subscription;
+
+
+    constructor(private loggedIn: LoginDataService, private tokenStorage: TokenStoreService,private carritoService: CarritoService) {
+    }
+
+    ngOnInit(): void {
+      if (this.tokenStorage.getToken()) {
+        this.loggedIn.changeLogged(true);
+      }
+
+      this.subscription = this.loggedIn.isLoggedIn.subscribe(logged => this.isLoggedIn = logged)
     }
 
      @Input()
